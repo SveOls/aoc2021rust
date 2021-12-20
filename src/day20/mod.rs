cfe28fb706c32efb026eb406117da62ef713e366
SveOls <sverre.d.olsen@live.no>
2021-12-21 00:43:42 +0100

    ddupate

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -14,30 +14,42 @@ pub fn run(inp: &str) -> Result<(), Box<dyn std::error::Error>> {
 }
 
 
-pub fn run_a(mut lines: Lines<BufReader<File>>) -> Result<(), Box<dyn std::error::Error>> {
-    let iterations = 10;
+pub fn run_a(lines: Lines<BufReader<File>>) -> Result<(), Box::<dyn std::error::Error>> {
+    println!("day 20a result: {}", runner(lines, 2)?);
+    Ok(())
+}
+
+pub fn run_b(lines: Lines<BufReader<File>>) -> Result<(), Box::<dyn std::error::Error>> {
+    println!("day 20b result: {}", runner(lines, 50)?);
+    Ok(())
+}
+
+
+pub fn runner(mut lines: Lines<BufReader<File>>, iterations: usize) -> Result<usize, Box<dyn std::error::Error>> {
+    let extra_space = iterations+2;
 
     let enhancement = lines.next().unwrap().unwrap().chars().map(|x| x == '#').collect::<Vec<bool>>();
     lines.next();
     let mut image = lines
         .map(|x| x.unwrap())
         .map(|x| x.chars()
-            .fold(vec![false; 2*(iterations + 1)], |mut tot, x| { tot.push(x == '#'); tot }))
+            .fold(vec![false; extra_space], |mut tot, x| { tot.push(x == '#'); tot }))
         .collect::<Vec<Vec<bool>>>();
-    image.append(&mut vec![vec![false; image[0].len()]; 2*(2*(iterations+1))]);
-    image.iter_mut().for_each(|x| (*x).append(&mut vec![false; 2*(iterations + 1)]));
-    image.rotate_right(2*(iterations + 1));
+    image.append(&mut vec![vec![false; image[0].len()]; 2*extra_space]);
+    image.iter_mut().for_each(|x| (*x).append(&mut vec![false; extra_space]));
+    image.rotate_right(extra_space);
 
     let mut indices = vec![vec![0; image[0].len()]; image.len()];
     let h = image.len();
     let w = image[0].len();
     let mut outside_world = false;
 
-
-    for _ in 0..iterations {
-        for (i, y ) in image.iter().enumerate() {
-            for (j, &x) in y.iter().enumerate() {
-                if x {
+    for itera in 0..iterations {
+        indices.iter_mut().for_each(|x| x.iter_mut().for_each(|x| *x = 0));
+        let offset = extra_space-2-itera;
+        for i in offset..h-offset {
+            for j in offset..w-offset {
+                if image[i][j] {
                     for i_1 in i.max(1)-1..(i+2).min(h) {
                         for j_1 in j.max(1)-1..(j+2).min(w) {
                             indices[i_1][j_1] += 2usize.pow((3*((i_1+1)-i) + ((j_1+1)-j)) as u32);
@@ -51,29 +63,34 @@ pub fn run_a(mut lines: Lines<BufReader<File>>) -> Result<(), Box<dyn std::error
         } else {
             enhancement[0]
         };
-        for i in 0..h {
-            for j in 0..w {
-                image[i][j] = enhancement[indices[i][j]];
-                indices[i][j] = 0;
+        for i in offset-1..h-offset+1 {
+            if i == offset || i == h-offset-1 || i == offset - 1 || i == h-offset {
+                for j in offset-1..w-offset+1 {
+                    image[i][j] = outside_world;
+                }   
+            } else {
+                image[i][offset-1] = outside_world;
+                image[i][offset] = outside_world;
+                image[i][w-offset-1] = outside_world;
+                image[i][w-offset] = outside_world;
+                for j in offset+1..w-offset-1 {
+                    image[i][j] = enhancement[indices[i][j]];
+                }
             }
         }
-        
-
-        // print_img(&image);
+        // _print_img(&image);
     }
 
-    
     let mut count = 0;
-    for i in 2*iterations..h-2*iterations {
-        for j in 2*iterations..h-2*iterations {
+    for i in 2..h-2 {
+        for j in 2..w-2 {
             if image[i][j] {
                 count += 1;
             }
         }
     }
-    println!("{}", count);
 
-    Ok(())
+    Ok(count)
 }
 
 
@@ -91,68 +108,3 @@ fn _print_img(image: &Vec<Vec<bool>>) {
     }
     println!()
 }
-
-
-
-pub fn run_b(mut lines: Lines<BufReader<File>>) -> Result<(), Box<dyn std::error::Error>> {
-    let iterations = 50;
-
-    let enhancement = lines.next().unwrap().unwrap().chars().map(|x| x == '#').collect::<Vec<bool>>();
-    lines.next();
-    let mut image = lines
-        .map(|x| x.unwrap())
-        .map(|x| x.chars()
-            .fold(vec![false; 2*(iterations + 1)], |mut tot, x| { tot.push(x == '#'); tot }))
-        .collect::<Vec<Vec<bool>>>();
-    image.append(&mut vec![vec![false; image[0].len()]; 2*(2*(iterations+1))]);
-    image.iter_mut().for_each(|x| (*x).append(&mut vec![false; 2*(iterations + 1)]));
-    image.rotate_right(2*(iterations + 1));
-
-    let mut indices = vec![vec![0; image[0].len()]; image.len()];
-    let h = image.len();
-    let w = image[0].len();
-    let mut outside_world = false;
-
-
-    for _ in 0..iterations {
-        for (i, y ) in image.iter().enumerate() {
-            for (j, &x) in y.iter().enumerate() {
-                // if x && i > iteration && j > iteration && (w-j) > iteration && (h-i) > iteration {
-                if x {
-                    for i_1 in i.max(1)-1..(i+2).min(h) {
-                        for j_1 in j.max(1)-1..(j+2).min(w) {
-                            // println!("{} {} {} {}", i, j, i_1, j_1);
-                            indices[i_1][j_1] += 2usize.pow((3*((i_1+1)-i) + ((j_1+1)-j)) as u32);
-                        }
-                    }
-                }
-            }
-        }
-        outside_world = if outside_world {
-            enhancement[511]
-        } else {
-            enhancement[0]
-        };
-        for i in 0..h {
-            for j in 0..w {
-                image[i][j] = enhancement[indices[i][j]];
-                indices[i][j] = 0;
-            }
-        }
-    }
-
-    
-    let mut count = 0;
-    for i in iterations+2..h-iterations-2 {
-        for j in iterations+2..h-iterations-2 {
-            if image[i][j] {
-                count += 1;
-            }
-        }
-    }
-    println!("{}", count);
-
-    Ok(())
-}
-
-
