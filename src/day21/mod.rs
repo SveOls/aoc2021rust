c7b7139cb8c988a95ca9c5776e15ae97a189eb39
SveOls <sverre.d.olsen@live.no>
2021-12-23 01:20:37 +0100

    update day21

--- src/day21/mod.rs
+++ src/day21/mod.rs
@@ -9,10 +9,110 @@ pub fn run(inp: &str) -> Result<(), Box<dyn std::error::Error>> {
     run_a(file_handling::get_file(inp)?)?;
 
     run_b(file_handling::get_file(inp)?)?;
+    
+
+    
+    use std::time::Instant;
+    let tests = [1000, 100000, 1000000];
+    for &i in &tests {
+        println!();
+        let before_any = Instant::now();
+        run_a_o1(file_handling::get_file(inp)?, true, i)?;
+        let after_skip = before_any.elapsed();
+        run_a_o1(file_handling::get_file(inp)?, false, i)?;
+        let after_non_skip = before_any.elapsed();
+        println!("doing {} iterations with skips took {} micros", i, after_skip.as_micros());
+        println!("doing {} iterations without skips took {} micros", i, (after_non_skip - after_skip).as_micros());
+        println!();
+    }
+
 
     Ok(())
 
 }
+
+
+pub fn run_a_o1(mut lines: Lines<BufReader<File>>, skip: bool, turns: usize) -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut pos_1: usize = lines.next().unwrap().unwrap().split(|x| x == ':').filter_map(|x|x.split_whitespace().collect::<String>().parse().ok() ).next().unwrap();
+    let mut pos_2: usize = lines.next().unwrap().unwrap().split(|x| x == ':').filter_map(|x|x.split_whitespace().collect::<String>().parse().ok() ).next().unwrap();
+
+    let score = [6, 5, 4, 3, 2, 1, 0, 9, 8, 7];
+
+    let goal = turns;
+
+    let mut score_1 = 0;
+    let mut score_2 = 0;
+
+    let mut score_pos = 0;
+    // hashmap of (pos_1, pos_2, last roll, who's up next) to (score, score, round number)
+    let mut states: HashMap<(usize, usize, usize, bool), (usize, usize, usize)> = HashMap::new();
+    let mut skipped = !skip;
+    let mut round = 0;
+    //
+    for _ in 0.. {
+        round += 1;
+        // one for before any rounds start, and after player 2 has made their turn
+        // if you're unfamilaiar with rust, this asks whether "states" contains 
+        // this key already. If so, it returns the value of the hashmap as "a" by reference.
+        if !skipped {
+            if let Some((old_score_1, old_score_2, old_round)) = states.get(&(pos_1, pos_2, score_pos%10, true)) {
+                // we can now safely iterate the score with whatever multiple of (score_1 - old_score_1) and (score_2 - old_score_2)
+                // we want. So let's do that. To prove we were here: 
+                println!("Skipping forward in time");
+                // and fast forwarding:
+
+                // the -1 is because if you're at 950/1000, and you can go exactly 50 forward, you'd overshoot by landing on 1000. 
+                let turns_forward_1 = (goal - score_1 - 1)/(score_1 - old_score_1);
+                let turns_forward_2 = (goal - score_2 - 1)/(score_2 - old_score_2);
+
+                // both scores must be increased by the same number of rounds. to avoid overshooting, im sticking to
+                // the lowest possible skip.
+                let max_turns_forward = turns_forward_1.min(turns_forward_2);
+
+                score_1 += max_turns_forward * (score_1 - old_score_1);
+                score_2 += max_turns_forward * (score_2 - old_score_2);
+                round   += max_turns_forward * (round   - old_round  );
+
+                // I've already skipped forward, skipping again won't help. Probably unncessary, but who knows what 
+                // bugs might show up.
+                skipped = true;
+    
+    
+            } else {
+            // if it doesn't have the value, i insert it to save it for later.
+                states.insert((pos_1, pos_2, score_pos%10, true), (score_1, score_2, round));
+            }
+            //
+        }
+
+
+        pos_1 = (pos_1 + score[score_pos%10]-1)%10+1;
+        score_pos += 1;
+        score_1 += pos_1;
+
+        
+        // you could insert another one here. I won't since it will never do anything in this case.
+        // states.insert((pos_1, pos_2, score_pos, false), (score_1, score_2));
+        //
+
+        pos_2 = (pos_2 + score[score_pos%10]-1)%10+1;
+        score_pos += 1;
+
+
+        if score_1 >= goal {
+            println!("Test result: {}", score_2 * (6*(round)-3));
+            break;
+        }
+        score_2 += pos_2;
+        if score_2 >= goal {
+            println!("Test result: {}", score_1 * 6*(round));
+            break;
+        }
+    }
+    Ok(())
+}
+
 
 
 pub fn run_a(mut lines: Lines<BufReader<File>>) -> Result<(), Box<dyn std::error::Error>> {
